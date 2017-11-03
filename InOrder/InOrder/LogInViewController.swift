f0b67004603d858fc1e314d17605c76218ae06d7
Brent Fordham <brentfordham@Brents-MacBook-Pro.local>
2017-11-03 10:50:26 -0400

    Started on code to handle authentication errors

--- InOrder/InOrder/LogInViewController.swift
+++ InOrder/InOrder/LogInViewController.swift
@@ -68,7 +68,25 @@ class LogInViewController: UIViewController {
     }
     
     @IBAction func logIn(_ sender: UIButton) {
-        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!, completion: nil)
+        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!, completion: {(user, error) in
+            if error != nil {
+                let errorCode = AuthErrorCode(rawValue: error!._code)
+                if let code = errorCode {
+                    switch code {
+                    case .invalidEmail:
+                        print("invalid email")
+                    case .wrongPassword:
+                        print("wrong password")
+                    case .userNotFound:
+                        print("user not found")
+                    case .userDisabled:
+                        print("user disabled")
+                    default:
+                        print(error!)
+                    }
+                }
+            }
+        })
         
     }
     
